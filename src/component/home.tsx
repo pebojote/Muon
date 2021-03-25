1f2e20518acb4f2cddbff5cc6a1bff1f6ddbf008
workpebojot <pebojot.edison.work@outlook.com>
2021-03-25 16:13:26 +0800

    Add Search/Explore Filter (#2)
    I added search or explore filter that let users to filter the results that showed in the page. I added search or explore filter to minimize complexity of finding relevant results.

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,100 +1,241 @@
-import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import BookCard from './templates/card';
-import androidImage from './assets/images/book cover/android.png';
-import cplusImage from './assets/images/book cover/cplus.png';
-import iosImage from './assets/images/book cover/ios.png';
-import javaImage from './assets/images/book cover/java.png';
-import javascriptImage from './assets/images/book cover/javascript.png';
-import linuxImage from './assets/images/book cover/linux.png';
-import pythonImage from './assets/images/book cover/python.png';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import LanguageIcon from '@material-ui/icons/Language';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import CodeIcon from '@material-ui/icons/Code';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import StarIcon from '@material-ui/icons/Star';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
+import ErrorIcon from '@material-ui/icons/Error';
+import UpdateIcon from '@material-ui/icons/Update';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import SortIcon from '@material-ui/icons/Sort';
+import {
+  createStyles,
+  makeStyles,
+  useTheme,
+  Theme,
+  fade,
+} from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import exploreData from './data/api.data';
+import Offline from './templates/offline';
+import FilterDialog from './templates/dialog.filter';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
+    search: {
+      margin: 5,
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.black, 0.15),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
     },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
     },
   })
 );
 
-const books = [
-  {
-    image: androidImage,
-    name: 'Android',
-    description:
-      'Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets.',
-    background: '#a4c639',
-    color: '#ffffff',
-  },
-  {
-    image: cplusImage,
-    name: 'C++',
-    description:
-      'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". ',
-    background: '#3f2b96',
-    color: '#ffffff',
-  },
-  {
-    image: iosImage,
-    name: 'iOS',
-    description:
-      'iOS is a mobile operating system created and developed by Apple Inc. exclusively for its hardware.',
-    background: '#D3CCE3',
-    color: '#ffffff',
-  },
-  {
-    image: javaImage,
-    name: 'Java',
-    description:
-      'Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.',
-    background: '#c94b4b',
-    color: '#ffffff',
-  },
-  {
-    image: javascriptImage,
-    name: 'JavaScript',
-    description:
-      'JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.',
-    background: '#FDC830',
-    color: '#ffffff',
-  },
-  {
-    image: linuxImage,
-    name: 'Linux',
-    description:
-      'Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.',
-    background: '#f5af19',
-    color: '#ffffff',
-  },
-  {
-    image: pythonImage,
-    name: 'Python',
-    description:
-      'Python is an interpreted, high-level and general-purpose programming language.',
-    background: '#00467F',
-    color: '#ffffff',
-  },
-];
-
-export default function home() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+export default function Home() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [data, setData] = useState('');
+  const [LibraryLength, setLibraryLength] = useState(
+    <CircularProgress size={10} />
+  );
+  const [isOffline, setNetworkStatus] = useState(false);
+
+  let [filter, setFilter] = useState([]);
+
+  const filteredData = (filtered) => {
+    setFilter(filtered);
+  };
+
+  useEffect(async () => {
+    try {
+      let mounted = true;
+      if (mounted) {
+        const data = await exploreData(filter);
+        if (data === 'offline') {
+          setNetworkStatus(true);
+        } else {
+          setData(data);
+          setLibraryLength(data.length);
+        }
+      }
+      return () => {
+        mounted = false;
+      };
+    } catch (error) {
+      console.log('Error: ', error.message);
+    }
+  }, [filter]);
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        {books.map((value, key) => (
-          <Grid item xs={4} key={key}>
-            <BookCard Book={value} />
+    <>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          fullWidth
+          placeholder="Search Libraries"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
+      <div style={{ width: '100%' }}>
+        <Box
+          display="flex"
+          alignItems="center"
+          p={1}
+          bgcolor="background.paper"
+        >
+          <Box p={1} flexGrow={1} bgcolor="grey.300">
+            {LibraryLength} libraries
+          </Box>
+          <Box p={1}>
+            <FilterDialog filteredData={filteredData} />
+          </Box>
+          <Box p={1}>
+            <Button href="#" style={{ textTransform: 'none' }}>
+              <SortIcon /> Sort by
+            </Button>
+          </Box>
+        </Box>
+      </div>
+      <Grid container spacing={1} style={{ marginTop: 10, padding: 10 }}>
+        {data ? (
+          data.map((value, key) => (
+            <Grid
+              key={key}
+              container
+              spacing={2}
+              style={{
+                marginTop: 10,
+                backgroundColor: '#ffffff',
+                borderRadius: '5px',
+              }}
+            >
+              <Grid item xs={8}>
+                <Paper style={{ padding: 5 }}>
+                  <Typography variant="h5">{value.name}</Typography>
+                  <Divider style={{ marginTop: 2, marginBottom: 2 }} />
+                  {value.topic.map((value, key) => (
+                    <Chip key={key} icon={<CheckCircleIcon />} label={value} />
+                  ))}
+                  <Divider style={{ marginTop: 3, marginBottom: 4 }} />
+                  <Typography>{value.description}</Typography>
+                  <Divider style={{ marginTop: 4, marginBottom: 3 }} />
+                  <Button href={value.web} style={{ textTransform: 'none' }}>
+                    <LanguageIcon /> Website
+                  </Button>
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <AssignmentIcon /> {value.license} License
+                  </Button>
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <CodeIcon /> {value.language}
+                  </Button>
+                  <Button href={value.sample} style={{ textTransform: 'none' }}>
+                    <PhotoLibraryIcon /> Sample
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper>
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <UpdateIcon /> {value.date.new}
+                  </Button>
+                  <br />
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <GetAppIcon /> {value.download} downloads
+                  </Button>
+                  <br />
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <StarIcon /> {value.star} stars
+                  </Button>
+                  <br />
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <CallSplitIcon /> {value.fork} forks
+                  </Button>
+                  <br />
+                  <Button
+                    href="#text-buttons"
+                    style={{ textTransform: 'none' }}
+                  >
+                    <ErrorIcon /> {value.issue} issues
+                  </Button>
+                </Paper>
+              </Grid>
+            </Grid>
+          ))
+        ) : isOffline ? (
+          <Grid container spacing={1} style={{ marginTop: 10, padding: 10 }}>
+            <Grid item xs={12}>
+              <Offline />
+            </Grid>
           </Grid>
-        ))}
+        ) : (
+          <Grid container spacing={1} style={{ marginTop: 10, padding: 10 }}>
+            <Grid item xs={12}>
+              <LinearProgress />
+            </Grid>
+          </Grid>
+        )}
       </Grid>
-    </div>
+    </>
   );
 }
